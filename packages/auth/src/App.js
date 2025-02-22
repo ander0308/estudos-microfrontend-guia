ecd81e38c3dda80395a2eb4ca1dd6355c8dc48d3
Anderson Santana <ander0308@yahoo.com.br>
2025-02-22 10:54:51 -0300

    feat: add funtion onsignIn

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { Switch, Route, Router } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { Switch, Route, Router } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-import Signin from './components/Signin';
-import Signup from './components/Signup';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'aut'
+  productionPrefix: "aut",
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <div>
       <Router history={history}>
         <StylesProvider generateClassName={generateClassName}>
           <Switch>
-            <Route path="/auth/signin" component={Signin} />
-            <Route path="/auth/signup" component={Signup} />
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </StylesProvider>
       </Router>
     </div>
-  )
+  );
 };
--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defautlHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defautlHistory, initialPath }) => {
   const history = defautlHistory || createMemoryHistory({
     initialEntries: [initialPath]
   });
@@ -13,7 +13,7 @@ const mount = (el, { onNavigate, defautlHistory, initialPath }) => {
   }
 
   ReactDOM.render(
-    <App history={history} />,
+    <App onSignIn={onSignIn} history={history} />,
     el
   );
 
